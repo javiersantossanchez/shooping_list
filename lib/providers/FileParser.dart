befe5666704b0ffa1dc0d07793f5773dbfccbf87
javier santos <javier.david.santos@gmail.com>
2020-04-24 15:33:20 -0500

    Refactoring to add extra validations

--- lib/providers/FileParser.dart
+++ lib/providers/FileParser.dart
@@ -15,16 +15,41 @@ class FileParser {
   final shoppingScheduleIdKey = "id";
 
   ShoppingScheduleItem _parse(dynamic item) {
+    DateTime date;
+    if(item[shoppingScheduleDateKey] == null){
+      print("The shoppingDate property is no present");
+      return null;
+    }
+    try {
+      date = DateTime.parse(item[shoppingScheduleDateKey]);
+    } on FormatException catch(exception){
+      print("Date has an invalid format $exception");
+      return null;
+    }
     List<dynamic> shoppingList = item[shoppingItemListKey];
+    if(shoppingList == null){
+      return null;
+    }
     List<ShoppingListItem> listItems = shoppingList.map((item) => ShoppingListItem(item[shoppingItemDescriptionKey])).toList();
-    DateTime date = DateTime.parse(item[shoppingScheduleDateKey]);
     return ShoppingScheduleItem(item[shoppingScheduleIdKey], date, listItems);
   }
 
   List<ShoppingScheduleItem> parser (String text) {
-    print(text);
-    List<dynamic> jsonFileContent = json.decode(text);
-    return jsonFileContent.map((item) => _parse(item)).toList();
+    List<dynamic> jsonFileContent;
+    if(text == null || text.isEmpty){
+      return null;
+    }
+    try {
+      jsonFileContent = json.decode(text);
+    } on FormatException catch(exception){
+      print("The file has invalid format $exception");
+      return null;
+    }
+    List<ShoppingScheduleItem> result = jsonFileContent.map((item) => _parse(item)).where((item) => item != null).toList();
+    if(result.isEmpty){
+      return null;
+    }
+    return result;
   }
 
 }
