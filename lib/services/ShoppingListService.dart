de3d352b4a1a0239e9593414ab1daca2e69ccd21
javier santos <javier.david.santos@gmail.com>
2020-06-07 13:18:09 -0500

    Add feature of connectivity with jexia services
    Add all functionality to get the default list from jexia platform.

--- lib/services/ShoppingListService.dart
+++ lib/services/ShoppingListService.dart
@@ -1,26 +1,41 @@
+import 'dart:convert';
+
 import 'package:shoopinglist/dtos/ShoppingItem.dart';
 import 'package:shoopinglist/dtos/ShoppingScheduleItem.dart';
 import 'package:shoopinglist/providers/FileDataProvider.dart';
 
-import 'package:shoopinglist/providers/IFileDataProvider.dart';
+import 'package:shoopinglist/providers/IDataProvider.dart';
+
+import 'package:shoopinglist/providers/IDefaultShoppingListDataProvider.dart';
+import 'package:shoopinglist/providers/JexiaDefaultShoppingListDataProvider.dart';
 
 class ShoppingListService {
 
 
-  final IFileDataProvider dataProvider;
+  final IDataProvider _dataProvider;
+
+  final IDefaultShoppingListDataProvider _defaultShoppingListDataProvider;
+
+
+
+  ShoppingListService({IDataProvider dataProvider, IDefaultShoppingListDataProvider defaultShoppingListDataProvider}):
+        this._dataProvider = dataProvider ?? new FileDataProvider() ,
+        this._defaultShoppingListDataProvider = defaultShoppingListDataProvider?? new JexiaDefaultShoppingListDataProvider();
 
   Future<List<ShoppingScheduleItem>> loadShoppingDays() async {
-    return await  dataProvider.getScheduler();
+    return await this._dataProvider.getScheduler();
   }
-
-  ShoppingListService({IFileDataProvider dataProvider}): this.dataProvider = dataProvider ?? new FileDataProvider();
 
   Future<List<ShoppingItem>> loadShoppingList(int id){
-    return this.dataProvider.getShoppingList(id);
+    return this._dataProvider.getShoppingList(id);
   }
 
 
-  List<ShoppingItem> getDefaultShoppingList(){
+  Future<List<ShoppingItem>> getDefaultShoppingList() async{
+    return this._defaultShoppingListDataProvider.getDefaultShoppingList();
+
+/*
+
     List<ShoppingItem> listToReturn = new List();
     listToReturn.add(new ShoppingItem("Arroz",false));
     listToReturn.add(new ShoppingItem("Pasta",false));
@@ -28,7 +43,11 @@ class ShoppingListService {
     listToReturn.add(new ShoppingItem("Pasta de dientes",false));
     listToReturn.add(new ShoppingItem("Cafe",false));
     listToReturn.add(new ShoppingItem("Papas",false));
-    listToReturn.add(new ShoppingItem("Aceite de oliva",false));
+    listToReturn.add(new ShoppingItem("Aceite de oliva",falseimport 'package:http/http.dart' as http;
+));
+
+
+
     listToReturn.add(new ShoppingItem("Banana",false));
     listToReturn.add(new ShoppingItem("Fresa",false));
     listToReturn.add(new ShoppingItem("Tomate",false));
@@ -38,6 +57,11 @@ class ShoppingListService {
     listToReturn.add(new ShoppingItem("Atun",false));
     listToReturn.add(new ShoppingItem("Diablito",false));
     listToReturn.add(new ShoppingItem("Zanahorias",false));
+
+
+
+
+
     listToReturn.add(new ShoppingItem("Cebolla",false));
     listToReturn.add(new ShoppingItem("Juegos",false));
     listToReturn.add(new ShoppingItem("Jamon",false));
@@ -47,6 +71,13 @@ class ShoppingListService {
     listToReturn.add(new ShoppingItem("Carne molida",false));
     listToReturn.add(new ShoppingItem("Pollo",false));
     listToReturn.add(new ShoppingItem("Bisteck",false));
+
+
+
+
+
+
+
     listToReturn.add(new ShoppingItem("Shampo",false));
     listToReturn.add(new ShoppingItem("Acondicionador",false));
     listToReturn.add(new ShoppingItem("Jabon de bañarse",false));
@@ -56,26 +87,29 @@ class ShoppingListService {
     listToReturn.add(new ShoppingItem("Harina de trigo",false));
     listToReturn.add(new ShoppingItem("Mantequilla de mani",false));
     listToReturn.add(new ShoppingItem("Mantequilla",false));
+
+
+
     listToReturn.add(new ShoppingItem("Cereal",false));
     listToReturn.add(new ShoppingItem("Compota",false));
     listToReturn.add(new ShoppingItem("Mandarina",false));
     listToReturn.add(new ShoppingItem("Aguacate",false));
     listToReturn.add(new ShoppingItem("Toallitas clorox",false));
-    listToReturn.add(new ShoppingItem("Desinfectante",false));
-    return listToReturn;
+    listToReturn.add(new ShoppingItem("Desinfectante",false));*/
+    //return listToReturn;
   }
 
   void createSchuelde(DateTime selectedDate, List<ShoppingItem> listItem ) {
     ShoppingScheduleItem newShoppingList = (ShoppingScheduleItemBuilder((new DateTime.now()).millisecondsSinceEpoch,selectedDate)..shoppingList = listItem).build();
-    dataProvider.createNewShoppingList(newShoppingList);
+    _dataProvider.createNewShoppingList(newShoppingList);
   }
 
   void deleteSchuelde(ShoppingScheduleItem shoppingListToDelete) {
-    dataProvider.deleteShoppingList(shoppingListToDelete);
+    _dataProvider.deleteShoppingList(shoppingListToDelete);
   }
 
   void updateShoopingItem(){
-    dataProvider.updateShoppingList();
+    _dataProvider.updateShoppingList();
   }
 
 }
